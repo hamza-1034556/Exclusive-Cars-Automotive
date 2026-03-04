a4695df19ed8352dbf205b601e3a05476991def9
Hamza El Kaddouri <1034556@hr.nl>
2026-03-04 22:19:47 +0100

    Update new car test

--- app.py
+++ app.py
@@ -74,6 +74,31 @@ CARS = [
         ],
         "description": "De koning van de hyper hatch, een legendarisch blok met het beste geluid van 2026.",
     },
+    {
+        "id": 3,
+        "name": "VW Golf 8.5 GTI Clubsport 2025",
+        "year": 2025,
+        "price_per_day": 300,
+        "deposit": 1500,
+        "min_age": 21,
+        "km_included": "Km vrij",
+        "location": "Rotterdam",
+        "fuel": "Benzine",
+        "transmission": "Automaat",
+        "zero_to_hundred": 5.6,
+        "top_speed": 250,
+        "images": [
+            "img/cars/golf-85-r-clubsport/01.jpg",
+            "img/cars/golf-85-r-clubsport/02.jpg",
+            "img/cars/golf-85-r-clubsport/03.jpg",
+        ],
+        "description": (
+            "Nieuw in ons assortiment: de VW Golf 8.5 GTI Clubsport 2025.\n\n"
+            "Deze verse Clubsport is precies wat je zoekt als je een sportieve hatchback wilt rijden met een scherp prijskaartje.\n\n"
+            "De Clubsport biedt een perfecte balans tussen dagelijks comfort en sportieve prestaties, met een 2.0 TSI motor die 300 pk levert en in slechts 5,6 seconden van 0 naar 100 km/u accelereert.\n\n"
+            "Probeer hem vandaag nog en ervaar de perfecte combinatie van stijl, comfort en sportiviteit in één hatchback."
+        ),
+    },
 ]
 
 
